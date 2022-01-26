7b30c7e1b019683bd0c7c74126a65c0c94c16f70
yehaoyang <dsreshiram@gmail.com>
2022-01-26 19:08:11 +0800

    fix

--- controllers/visitor.go
+++ controllers/visitor.go
@@ -23,7 +23,7 @@ func GetVisitorInfo(ctx *gin.Context) {
 		info.IP = ctx.GetHeader("X-Forwarded-For")
 	}
 	info.UserAgent = ctx.GetHeader("User-Agent")
-	info.Host = ctx.GetHeader("Host")
+	info.Host = ctx.Request.Host
 	frequency := models.VisitorInfo(info)
 	ctx.Writer.WriteString(fmt.Sprintf("%d", frequency))
 }
